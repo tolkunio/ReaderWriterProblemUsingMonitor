7502a8850dc4410757b6c7f6724d741514f7f849
Omurbekova Tolkun <tolkun3922@gmail.com>
2017-05-03 09:12:19 +0600

    Решение проблем читателя-писателя с помощью монитора на

---
+++ Cinematix01/MainFormEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cinematix01
+{
+    public class MainFormEventArgs : MouseEventArgs
+    {
+        public MainForm MainForm { get; set; }
+        public MainFormEventArgs(MouseButtons button, int clicks, int x, int y, int delta) : base(button, clicks, x, y, delta)
+        {
+
+        }
+
+    }
+}
